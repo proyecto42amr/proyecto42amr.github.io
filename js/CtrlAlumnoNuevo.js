710dd9b58bf286b9e894459179451aaece9ff627
proyecto42amr <82295696+proyecto42amr@users.noreply.github.com>
2021-04-10 23:04:41 -0500

    Update CtrlAlumnoNuevo.js

--- js/CtrlAlumnoNuevo.js
+++ js/CtrlAlumnoNuevo.js
@@ -42,7 +42,7 @@ async function guarda(evt) {
         formData, "matricula").trim();  
     const nombre = getString(
       formData, "nombre").trim();
-    const telefono = getInt(
+    const telefono = getString(
       formData, "telefono").trim();
     const grupo = getString(
       formData, "grupo").trim();
