ab57382a8917841c98799219693990b5f5ac19d6
proyecto42amr <82295696+proyecto42amr@users.noreply.github.com>
2021-04-13 22:26:00 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -4,11 +4,10 @@ class MiFooter
     this.innerHTML = /* html */
       `<p>
         &copy; 2021
-        Arely Montes Resendiz.
+        Arely Montes Resendiz
       </p>`;
   }
 }
-
 customElements.define(
   "mi-footer", MiFooter);
 © 2021 GitHub, Inc.
